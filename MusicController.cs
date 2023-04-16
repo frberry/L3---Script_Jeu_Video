4651363fa3a5709daf4074c8f665b5512176fdc0
César Berry <123377075+cesarbrry@users.noreply.github.com>
2023-04-16 15:34:01 +0200

    Update MusicController.cs

--- MusicController.cs
+++ MusicController.cs
@@ -23,7 +23,7 @@ public class MusicController : MonoBehaviour
         volume=0.5f;
         pitch=1f;
     }
-    // Start est appelé en premier
+    // Start is called before the first frame update
     void Start()
     {
         source.clip=sound;
@@ -36,7 +36,7 @@ public class MusicController : MonoBehaviour
 
     }
 
-    // Update est appelé a chaque fois que le jeu refresh
+    // Update is called once per frame
     void Update()
     {
         if(UnityEngine.Input.GetKeyDown("space"))
